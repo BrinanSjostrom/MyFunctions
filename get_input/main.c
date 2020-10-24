ed8d8e877a875aa8179a65df2429c261f6df127a
grim-ops <grim-ops@protonmail.com>
2020-10-24 02:09:40 -0500

    Added more testing for multiple inputs

--- get_input/main.c
+++ get_input/main.c
@@ -5,9 +5,13 @@
 int main()
 {
 	char *input;
+	char *age;
 	printf("What is your name: ");
-	size_t name_len = get_input(&input, 10);
-	printf("\nHello %s!\nYour name is %d letters long!\n", input, name_len);
+	size_t name_len = get_input(&input, 1);
+	printf("How old are you: ");
+	get_input(&age, 1);
+	printf("\n\n\nHello %s!\nYour name is %d letters long!\nYou are %s years old!\n", input, name_len, age);
 	free(input);
+	free(age);
 	return 0;
 }
