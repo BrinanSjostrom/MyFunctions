5c787aed51b97fc0fbc5f57ea86d6753c42a1a2a
grim-ops <grim-ops@protonmail.com>
2020-10-24 02:10:10 -0500

    Rewrote function so there are less bugs

--- get_input/get_input.c
+++ get_input/get_input.c
@@ -5,33 +5,28 @@
 
 size_t get_input(char **input, const size_t buffer_size)
 {
-	char buffer[buffer_size + 1];						// Buffer (size is indicated by the programmer)
-	memset(buffer, 0, sizeof(buffer));			// Sets buffer to all 0s
-	size_t size_of_input = 1;								// Inits size of the user input
-	unsigned short int buffer_index;				// This variable is used to track the index were at
-	*input = (char*)malloc(size_of_input);	//	Allocates space for the input
+
+	char buffer[buffer_size + 1];
+
+	size_t size_of_input = 1;
+	*input = (char*)malloc(sizeof(char) * size_of_input);
 	strcpy(*input, "");
 
 	while(1)
 	{
-		char ch = fgetc(stdin);
-		if(ch == '\n')																					// Turns buffer into workable string
-			ch = '\0';
-		buffer[buffer_index] = ch;
-		buffer_index++;
-		if(ch == '\0'){																					// checks to see if its the end of stdin
-			size_of_input += strlen(buffer);
-			*input = (char*)realloc(*input, size_of_input);
-			strcat(*input, buffer);
-			return size_of_input - 1;
-		}
-		else if(buffer_index == buffer_size)										// checks to see if the buffer is maxed out
+		memset(buffer, 0, sizeof(buffer));
+		for(int i = 0; i < buffer_size; i++)
 		{
-			buffer[buffer_size] = '\0';
-			size_of_input += buffer_size;
-			*input = (char*)realloc(*input, size_of_input);
-			strcat(*input, buffer);
-			buffer_index = 0;
+			buffer[i] = fgetc(stdin);
+			if(buffer[i] == '\n' || buffer[i] == EOF || buffer[i] == '\0'){
+				buffer[i] = '\0';
+				break;
+			}
 		}
+		size_of_input+=strlen(buffer);
+		*input = (char*)realloc(*input, sizeof(char) * size_of_input);
+		strcat(*input, buffer);
+		if(strlen(buffer) != buffer_size)
+			return size_of_input - 1;
 	}
 }
